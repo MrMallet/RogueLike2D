4bd7f7a8126c2bcd92dd98a01068639468fd6ee4
MrMallet <g00304978@gmit.ie>
2015-12-20 13:10:46 +0000

    second commit- player doesnt hit walls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+	public float levelStartDelay = 2f;
+	public float turnDelay = .1f;
+	public static GameManager instance = null; 
+	public int playerFoodPoints =100;
+	[HideInInspector] public bool playersTurn = true;
+
+	public BoardManager boardScript;
+
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool doingSetUp;
+
+	void Awake(){
+		if (instance == null) 
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		enemies = new List<Enemy> ();
+		boardScript = GetComponent<BoardManager>();
+		InitGame ();
+	}
+
+	private void OnLevelWasLoaded (int index){
+		level++;
+		InitGame ();
+	}
+
+	void InitGame(){
+		doingSetUp = true;
+
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text> ();
+		levelText.text = "Day " + level;
+		levelImage.SetActive (true);
+		Invoke ("HideLevelImage", levelStartDelay);
+
+		enemies.Clear ();
+		boardScript.SetupScene(level);
+	}
+
+	private void HideLevelImage(){
+		levelImage.SetActive (false);
+		doingSetUp = false;
+	}
+
+	public void GameOver(){
+		levelText.text = "After " + level + " days, you starved! ";
+		enabled = false;
+	}
+
+	void Update(){
+		if (playersTurn || enemiesMoving)
+			return;
+		StartCoroutine (MoveEnemies ());
+	}
+
+	public void AddEnemyToList(Enemy script){
+		enemies.Add (script);
+	}
+
+	IEnumerator MoveEnemies(){
+		enemiesMoving = true;
+		yield return new WaitForSeconds (turnDelay);
+
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds(turnDelay);
+		}
+		for (int i =0; i< enemies.Count; i++) {
+			enemies[i].MoveEnemy ();
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+}
